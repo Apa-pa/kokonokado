7e1cd70d0b69516a5313b9c4b2b45d852a072f40
Apa-pa <113153920+Apa-pa@users.noreply.github.com>
2023-01-30 14:44:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ $(function() {
   $('#menu-show').click(
     function(){
     $('#menu-modal').fadeIn();
+    $('.overlay').addClass("open");
   });
 
   $('#menu-close').click(
@@ -35,17 +36,59 @@ $(function() {
     $('#menu-modal').fadeOut();
   });
 
+  $('.overlay').click(
+    function(){
+      $('#menu-modal').fadeOut();
+      $('.overlay').removeClass("open");
+    }
+  )
+
+ 
+
 
 // スライド
-  $(".slide-items").slick({
+  $('.slide-items').slick({
     infinite: true,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
     dots: true,
     variableWidth: true,
-  });
+    prevArrow: '<img src="img/circle-chevron-left-solid.svg" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="img/circle-chevron-right-solid.svg" class="slide-arrow next-arrow">',
+});
+
     
+// 商品モーダル
+  $('#item-modal-1').click(
+    function(){
+      $('#modal-1-01').fadeIn();
+      $('#js-overlay').addClass("open");
+    }
+  );
+
+  $('#item-modal-2').click(
+    function(){
+      $('#modal-1-02').fadeIn();
+      $('#js-overlay').addClass("open");
+    }
+  );
+
+
+  $('.item-modal-close').click(
+    function(){
+      $('.item-modal').fadeOut();
+      $('#js-overlay').removeClass("open");
+    }
+  );
+
+  $('#js-overlay').click(
+    function(){
+      $('.item-modal').fadeOut();
+      $('#js-overlay').removeClass("open");
+    }
+  )
+
 
 
 
