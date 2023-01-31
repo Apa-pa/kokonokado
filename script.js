48c4baaf688e9c6b968e1b48043672211208ea4a
Apa-pa <113153920+Apa-pa@users.noreply.github.com>
2023-01-31 11:36:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,114 +1,129 @@
-$(function() {
+$(function(){
 
-// FAQ
-
-  $('.faq-list-item').click(
-    function(){
-      var $answer = $(this).find('.answer');
-      if ($answer.hasClass('open')) {
-        $answer.removeClass('open');
-        $(this).find('span').text('+');
-      }  else {
-        $answer.addClass('open');
-        $(this).find('span').text('-');
-
-      }
-    }
-  );
-
-// homeに戻る
-    $('#home-link').click(
+  // FAQ
+  
+    $('.faq-list-item').click(
       function(){
-        $('html,body').scrollTop(0);
-
+        var $answer = $(this).find('.answer');
+        if ($answer.hasClass('open')) {
+          $answer.removeClass('open');
+          $(this).find('span').text('+');
+        }  else {
+          $answer.addClass('open');
+          $(this).find('span').text('-');
+  
+        }
       }
     );
-
-// メニュー
-  $('#menu-show').click(
-    function(){
-    $('#menu-modal').fadeIn();
-    $('.overlay').addClass("open");
-  });
-
-  $('#menu-close').click(
-    function(){
-    $('#menu-modal').fadeOut();
-  });
-
-  $('.overlay').click(
-    function(){
+  
+  // homeに戻る
+      $('#home-link').click(
+        function(){
+          $('html,body').scrollTop(0);
+  
+        }
+      );
+  
+  // メニュー
+    $('#menu-show').click(
+      function(){
+      $('#menu-modal').fadeIn();
+      $('.overlay').addClass("open");
+    });
+  
+    $('#menu-close').click(
+      function(){
       $('#menu-modal').fadeOut();
-      $('.overlay').removeClass("open");
+    });
+  
+    $('.overlay').click(
+      function(){
+        $('#menu-modal').fadeOut();
+        $('.overlay').removeClass("open");
+      }
+    )
+  
+   
+  
+  
+  // スライド
+    $('.slide-items').slick({
+      infinite: true,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots: true,
+      variableWidth: true,
+      prevArrow: '<img src="img/circle-chevron-left-solid.svg" class="slide-arrow prev-arrow">',
+      nextArrow: '<img src="img/circle-chevron-right-solid.svg" class="slide-arrow next-arrow">',
+  });
+  
+  // 商品モーダルを開く
+  
+  $('.yakigashi').click(
+    function(){
+      var clickedIndex = $('.yakigashi').index($(this))-1;
+      $('.yakigashi-modal').eq(clickedIndex).fadeIn();
+      $('#js-overlay').addClass("open");
     }
-  )
-
- 
-
-
-// スライド
-  $('.slide-items').slick({
-    infinite: true,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    dots: true,
-    variableWidth: true,
-    prevArrow: '<img src="img/circle-chevron-left-solid.svg" class="slide-arrow prev-arrow">',
-    nextArrow: '<img src="img/circle-chevron-right-solid.svg" class="slide-arrow next-arrow">',
-});
-
+  );
+  
+  $('.food').click(
+    function(){
+      var clickedIndex = $('.food').index($(this))-1;
+       $('.food-modal').eq(clickedIndex).fadeIn();
+      $('#js-overlay').addClass("open");
+    }
+  );
+  
+  $('.shishu').click(
+    function(){
+      var clickedIndex = $('.shishu').index($(this))-1;
+       $('.shishu-modal').eq(clickedIndex).fadeIn();
+      $('#js-overlay').addClass("open");
+    }
+  );
+  
+      
+  // 商品モーダルを閉じる
     
-// 商品モーダル
-  $('#item-modal-1').click(
-    function(){
-      $('#modal-1-01').fadeIn();
-      $('#js-overlay').addClass("open");
-    }
-  );
-
-  $('#item-modal-2').click(
-    function(){
-      $('#modal-1-02').fadeIn();
-      $('#js-overlay').addClass("open");
-    }
-  );
-
-
-  $('.item-modal-close').click(
-    function(){
+    function modalClose(){
       $('.item-modal').fadeOut();
       $('#js-overlay').removeClass("open");
-    }
-  );
-
-  $('#js-overlay').click(
-    function(){
-      $('.item-modal').fadeOut();
-      $('#js-overlay').removeClass("open");
-    }
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+    };
+  
+    $('.item-modal-close').click(
+      function(){
+        modalClose();
+      }
+    );
+  
+    $('#js-overlay').click(
+      function(){
+        modalClose();
+      }
+    );
+  
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  });
